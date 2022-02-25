6749a6577e147038df9fa52bb96756e53f529d5c
Joshua Jakab <joshua.jakab@gmail.com>
2022-02-25 14:28:42 -0500

    shop now button

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import HomeImage from '../../media/download.jpg';
+import { useHistory } from 'react-router-dom';
+import Button from '../defaultComponents/Button';
 import './styles.css';
 
 const Home = props => {
+    const history = useHistory();
     return (
         <div className='home-container'>
             <div className='image-container'>
@@ -12,6 +15,10 @@ const Home = props => {
                 <h2 className='home-content'>
                 Welcome to Green Mountain Synergy where it is our mission to produce the highest quality products for the best value possible. We understand that whatever we produce and sell must be good for the planet, our employees, and our end consumer. We believe CBD should be affordable for everyone. We will always value people and our planet above profits. We hope you can feel our intent and the potency of nature in all of our amazing products. Thank you for your support! <br /> <br />
                 </h2>
+
+                <Button className='shop-button' onClick={() => history.push('/shop')}>
+                    <h2>SHOP NOW!</h2>
+                </Button>
                 
                 <h2 className='home-tag'>Real plants. Real people. Real value.</h2>
                 {/*<div className='blog-preview'>
